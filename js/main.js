d6771464f4dbe46b20e8ca27cc00a0491bd0afe0
Mahendra-t <bus.mahendrat@gmail.com>
2021-11-13 00:50:09 +0100

    added images, html modified, css modified

--- js/main.js
+++ js/main.js
@@ -17,16 +17,8 @@ window.addEventListener("load", () => {
 
     demoGame.start();
   }
-
-  let playBtn = document.querySelector("#play-btn");
-  playBtn.addEventListener("click", gameScreen);
-
+  
   let startBtn = document.querySelector("#start-btn");
   startBtn.addEventListener("click", startGame);
 
-  let winBtn = document.querySelector("#win-btn");
-  winBtn.addEventListener("click", winGameScreen);
-
-  let gameoverBtn = document.querySelector("#gameover-btn");
-  gameoverBtn.addEventListener("click", gameOverScreen);
 });
